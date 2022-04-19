c17b878879bb62769a2fa6c49e7ef231e715910f
Laise2609 <85645803+Laise2609@users.noreply.github.com>
2022-04-19 17:04:42 -0300

    api pra poder mandar as coisas p gmail

--- simplicity/js/scripts.js
+++ simplicity/js/scripts.js
@@ -75,3 +75,35 @@ textMensagem.addEventListener("input", function(){
        textMensagem.style.boxShadow = "black 0 0 10px"
     }
 });
+
+//Programação do Formspree
+var form = document.getElementById("my-form");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("my-form-status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        if (response.ok) {
+          status.innerHTML = "Obrigado pelo retorno!";
+          form.reset()
+        } else {
+          response.json().then(data => {
+            if (Object.hasOwn(data, 'errors')) {
+              status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+            } else {
+              status.innerHTML = "Oops! teve um erro ao enviar, tente novamente mais tarde"
+            }
+          })
+        }
+      }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form"
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
